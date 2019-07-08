3167fe3294f629fc5efb51ad7b657fa7aa8cb2ab
rrumang <rrumang@gmail.com>
2019-07-08 19:11:50 +0900

    springbatch2 초기

--- src/test/java/kr/or/ddit/batch/RangerBatchTest.java
+++ src/test/java/kr/or/ddit/batch/RangerBatchTest.java
@@ -9,20 +9,25 @@ import org.junit.runner.RunWith;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:kr/or/ddit/config/spring/application-batch-dev.xml"})
+@ContextConfiguration(locations = {
+		"classpath:kr/or/ddit/config/spring/application-datasource-dev.xml",
+		"classpath:kr/or/ddit/config/spring/application-transaction.xml",
+		"classpath:kr/or/ddit/config/spring/application-batch-dev.xml"})
 public class RangerBatchTest {
 
 	@Resource(name="jobLauncher")
 	private JobLauncher jobLauncher;
 	
-	@Resource(name="rangerJob")
-	private Job rangerJob;
+	@Resource(name="yogultJob")
+	private Job yogultJob;
 	
 	/**
 	 * 
@@ -30,14 +35,18 @@ public class RangerBatchTest {
 	* 작성자 : PC08
 	* 변경이력 :
 	* @throws Exception
-	* Method 설명 :레인저스 이름 수정 배치 테스트
+	* Method 설명 :발효유 일실적 생성 배치
 	 */
 	@Test
-	public void RangerBatchTest() throws Exception {
+	public void yogultBatchTest() throws Exception {
 		/***Given***/
+		String st = "201907";
 
 		/***When***/
-		JobExecution jobExcution = jobLauncher.run(rangerJob, new JobParameters());
+		JobExecution jobExcution = jobLauncher.run(yogultJob,
+				new JobParametersBuilder().addParameter("ym", new JobParameter(st))
+				.addLong("start_dt", System.currentTimeMillis())
+				.toJobParameters());
 
 		/***Then***/
 		assertEquals(ExitStatus.COMPLETED, jobExcution.getExitStatus());
